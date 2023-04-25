e876aa42a76e406563cf08eaea80152cdc265e30
joalissonmarques <joalisonmarques@hotmail.com>
2023-04-25 11:39:07 -0300

    insert user with POST

--- src/main/java/com/joalissonmarques/workshopmongo/resources/UserResource.java
+++ src/main/java/com/joalissonmarques/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.joalissonmarques.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.joalissonmarques.workshopmongo.domain.User;
 import com.joalissonmarques.workshopmongo.dto.UserDTO;
@@ -32,6 +36,17 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){	
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){	
+		//aqui convertemos o DTO para User
+		User obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		//Codigo especifico do sprint para retornar url do novo recurso criado, assim ele pega o endereço do novo
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		//created retorna o 201, que é de criação
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
