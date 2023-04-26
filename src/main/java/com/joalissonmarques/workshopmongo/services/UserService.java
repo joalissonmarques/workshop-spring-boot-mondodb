0de5df416eb07b7144511bc149f970c3d73924cc
joalissonmarques <joalisonmarques@hotmail.com>
2023-04-26 10:46:55 -0300

    update user

--- src/main/java/com/joalissonmarques/workshopmongo/services/UserService.java
+++ src/main/java/com/joalissonmarques/workshopmongo/services/UserService.java
@@ -29,12 +29,24 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+	
 	public void delete(String id) {
 		//fazemos ele fazer a busca priemiro, se ele não encontrar, já lança o erro
 		findById(id);
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
