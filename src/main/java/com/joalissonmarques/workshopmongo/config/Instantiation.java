9584540a96cef7c60e2d40706074658bc2dab2c5
joalissonmarques <joalisonmarques@hotmail.com>
2023-04-26 11:38:37 -0300

    Referencing user posts

--- src/main/java/com/joalissonmarques/workshopmongo/config/Instantiation.java
+++ src/main/java/com/joalissonmarques/workshopmongo/config/Instantiation.java
@@ -43,6 +43,10 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		//Para incluir os posts para maria
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 	}
 
--- src/main/java/com/joalissonmarques/workshopmongo/domain/User.java
+++ src/main/java/com/joalissonmarques/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.joalissonmarques.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //Podemos também colocar apenas Document e o spring mapea conforme o nome da class
@@ -15,6 +18,10 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	//Lazy estamos referenciando uma coleção, não queremos carregar os posts quando puxarmos/buscarmos o user para não sobrecarregar
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 
 	public User() {
 
@@ -50,6 +57,14 @@ public class User implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -67,5 +82,4 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
