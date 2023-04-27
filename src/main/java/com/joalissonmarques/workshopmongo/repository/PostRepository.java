5f34e418eca7ae70300dc4b881ec49643582471a
joalissonmarques <joalisonmarques@hotmail.com>
2023-04-27 10:58:22 -0300

    Simple query with methods

--- src/main/java/com/joalissonmarques/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/joalissonmarques/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.joalissonmarques.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.joalissonmarques.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	//Colocamos o IgnoreCase para ele aceitar maiuscula e minuscula
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
